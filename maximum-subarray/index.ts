8223b1b8cd8c2873a1ae01a147918ddac0ce8b84
jayjnu <doublejnu@gmail.com>
2017-02-27 00:47:09 +0900

    최대연속 부분합 해결

--- maximum-subarray/index.ts
+++ maximum-subarray/index.ts
@@ -9,23 +9,31 @@ const testCases: eachTest[] = TEST.cases;
 const sum: (a:number, b:number) => number = (a, b) => a + b;
 
 const MCS: (test: eachTest) => number = (test) => {
-    const testCases = test.content;
+    const arr = test.content;
     const size = test.size;
     let sum: number = 0;
-    let largest: number = 0;
+    let result: number = 0;
 
-    for(let i  = 0 ; i < size ; i++){
-        sum = sum + testCases[i];
-        largest = largest < sum ? sum : largest;
-        console.log(largest);
+    /*
+    vars arr, MAX, sum,
+    1. if(MAX + a[i] > MAX) MAX += a[i]
+    2. 
+    */
+
+    for (let i = 0 ; i < size ; i++){
+        sum += arr[i];
+        if(sum < 0){
+            sum = 0;
+        }else if(result < sum){
+            result = sum;
+        }
     }
-
-    return largest;
+    return result;
 };
 
 const testEach = (testCases: eachTest[]) => {
     testCases.forEach( (test, i) => {
-        if(i > 0)  return;
+        // if(i > 0)  return;
         describe(`TEST CASE ${i + 1}: [${test.content.join(',')}]`, function(){
             it(`should be equal to ${test.answer}`, function(){
                 expect(MCS(test)).to.equal(test.answer);
