f1884ea64c1c3a34fde91fb6e624e30fb68bdcdc
jayjnu <doublejnu@gmail.com>
2017-03-13 03:34:48 +0900

    made changes to nothing significant

--- travelling-salesman/index.ts
+++ travelling-salesman/index.ts
@@ -1,22 +1,34 @@
-import { expect } from 'chai';
-import { INPUT, Input } from './input';
-import { stringifyInput } from './util';
-
+import {expect} from 'chai';
+import {INPUT, Input} from './input';
+import {stringifyInput} from './util';
+/*
+ * Q. 모든 도시를 각각 한번만 거쳐 가장 최소한의 값이 나오도록 하는 루트 찾아서 최소한의 값 출력
+ *
+ *  Conditions
+ * 1.
+ * */
 const SolveProblem: (INPUT: Input) => number = (INPUT) => {
-    let result: number = 0;
+  let result: number = 0;
+  const citiesVisited = {};
 
-    return result;
+  // methods
+  interface Visit { (input: Input, mvnt?: number, from?: number, to?: number): number }
+  const visitCities: Visit = (input, mvnt = 0, from = 0, to = 0) => {
+
+    return 0;
+  };
+
+  return result;
 };
 
-
 const Test: () => void = () => {
-    describe('Salesman Problem Result', function(){
-        INPUT.forEach((test, idx) => {
-            it(`With Input \n${stringifyInput(test.input)}\n should print ${test.output}`, function(){
-                expect(SolveProblem(test)).to.equals(test.output);
-            });
-        });
+  describe('Salesman Problem Result', function () {
+    INPUT.forEach((test, idx) => {
+      it(`With Input \n${stringifyInput(test.input)}\n should print ${test.output}`, function () {
+        expect(SolveProblem(test)).to.equals(test.output);
+      });
     });
+  });
 };
 
 Test();
